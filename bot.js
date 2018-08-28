d64f89a32fab79ff66e794469f37568c033c2fe0
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-08-29 01:12:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ahmedGmr`,"http://twitch.tv/S-F")
+client.user.setGame(`Top$Bot$`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -26,31 +26,6 @@ client.user.setGame(`ahmedGmr`,"http://twitch.tv/S-F")
   console.log('╚[════════════]╝')
   console.log('')
   console.log('')
-});
-client.on('message', message => {
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`); 
-    if (!args[1]) {
-return;
-}
-        message.guild.members.forEach(m => {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-        var bc = new Discord.RichEmbed()
-            .addField(':earth_americas:  » سيرفر : ', message.guild.name)
-            .addField(':thinking:  » راسل : ', message.author.username)
-            .addField(':pencil:  » الرسالة : ', args)
-            .setColor('#ff0000')
-            // m.send(`[${m}]`);
-            m.send(`${m}`,{embed: bc});
-        });
-    }
-    } else {
-        return;
-    }
 });
 
 client.login(process.env.BOT_TOKEN);
