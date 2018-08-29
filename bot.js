a5889f90c8683cd6fbadff50c879d558772a7726
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-08-29 04:26:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ client.user.setGame(`Top$Bot$`,"http://twitch.tv/S-F")
 });
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + -bc")) {
+            if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' '); 
