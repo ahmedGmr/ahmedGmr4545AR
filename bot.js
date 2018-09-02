903595b3f12ebf631d2532969ac755685079788f
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-09-03 00:40:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,18 +27,31 @@ client.user.setGame(`Top$Bot$`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    message.channel.send(`\`${message.guild.memberCount}\` : عدد الاعضاء المستلمين`); 
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        var bc = new Discord.RichEmbed()
+            .addField(':earth_americas:  » سيرفر : ', message.guild.name)
+            .addField(':thinking:  » راسل : ', message.author.username)
+            .addField(':pencil:  » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
  message.delete(); 
-};     
-});
+        });
+    }
+    } else {
+        return;
+    }
+}); 
 
 client.login(process.env.BOT_TOKEN);
