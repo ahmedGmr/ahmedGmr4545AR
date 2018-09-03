6bccfe0a8446f7565500959fc5cc59961d47c41f
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-09-03 05:20:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Top$Bot$`,"http://twitch.tv/S-F")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -46,110 +46,11 @@ return;
             .setColor('#ff0000')
             // m.send(`[${m}]`);
             m.send(`${m}`,{embed: bc});
- message.delete(); 
         });
     }
     } else {
         return;
     }
-}); 
-client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix + "help-en") {
-         message.channel.send('**The Message Was Sent On Private**');
-            
-    
-         
-
-
- message.author.sendMessage(`
- **
-╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮
-┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱┃┃
-┃╰━━┳╮╭┳━━┳╮╭┳━━┫┃╭━━╮
-┃╭━━┻╋╋┫╭╮┃╰╯┃╭╮┃┃┃┃━┫
-┃╰━━┳╋╋┫╭╮┃┃┃┃╰╯┃╰┫┃━┫
-╰━━━┻╯╰┻╯╰┻┻┻┫╭━┻━┻━━╯
-╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
-╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
-
-╔[❖════════════❖]╗
-             Prefix = ' * '
-╚[❖════════════❖]╝
-
-╔[❖════════════❖]╗
-             Admin Commands
-╚[❖════════════❖]╝
-
- ❖ *kick <mention > ➾ kick member from server
-
- ❖ *clear ➾ clear chat ( not ready yet fixing)
- 
- ❖ *mute < mention > ➾ mute member
-
- ❖ *unmute <mention> ➾ unmute member
-
- ❖ *ct <name> ➾ create channel
-
- ❖ *cv <name> create voice channel
-  
- ❖ *bc <message> ➾ message all members in server
-
-
-╔[❖════════════❖]╗
-            General  Commands
-╚[❖════════════❖]╝
-
-❖ *roll <number> ➾ role 
-
-❖ *date ➾ see date
-
-❖ *member ➾ members info
-
-❖ *ser-av ➾ server avatar
-
-❖ *uptime ➾ to see uptime
-
-❖ *invs ➾ server invite link
-
-❖ *own ➾ bot owner
-
-❖ *id ➾ your id
-
-❖ *avatar ➾ your avatar account
-
-❖ *help-en ➾ help by arabic
-
-❖ *help-en ➾ help by english
-
-❖ *ping ➾ to see ping
-
-❖ *bot ➾ bot informations 
-
-❖ *server ➾ server informations 
-
-❖ *invite ➾ bot invite link
-
-╔[❖════════════❖]╗
-                    Welcome
-╚[❖════════════❖]╝
-
-to enable welcome message do channel name "wlc"
-
-
-==================================================================
-
-Server support:https://discord.gg/GKHMD7
-==================================================================
-
-bot invite link: https://discordapp.com/api/oauth2/authorize?client_id=471531013193859072&permissions=0&redirect_uri=https%3A%2F%2Fdiscord.gg%2F5amSGB&scope=bot
-
-==================================================================
-
-`);
-
-    }
 });
-
 
 client.login(process.env.BOT_TOKEN);
