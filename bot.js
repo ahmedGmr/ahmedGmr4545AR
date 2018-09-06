d9764cc8bdbb0029bf8913ce52fbed88aff029b1
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-09-06 14:54:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
 client.on('message', message => {
     if (message.author.bot) return;
      if (message.content === prefix + "help") {
