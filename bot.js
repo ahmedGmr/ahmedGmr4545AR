b5987284886359617f2879094233bd8d1965b0c3
ahmedGmr <42749455+ahmedGmr@users.noreply.github.com>
2018-09-06 09:54:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,105 @@ client.user.setGame(`Top$Bot$`,"http://twitch.tv/S-F")
   console.log('')
 });
 client.on('message', message => {
+    if (message.author.bot) return;
+     if (message.content === prefix + "help-en") {
+         message.channel.send('**The Message Was Sent On Private**');
+            
+    
+         
+
+
+ message.author.sendMessage(`
+ **
+╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╭╮
+┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱┃┃
+┃╰━━┳╮╭┳━━┳╮╭┳━━┫┃╭━━╮
+┃╭━━┻╋╋┫╭╮┃╰╯┃╭╮┃┃┃┃━┫
+┃╰━━┳╋╋┫╭╮┃┃┃┃╰╯┃╰┫┃━┫
+╰━━━┻╯╰┻╯╰┻┻┻┫╭━┻━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+
+╔[❖════════════❖]╗
+             Prefix = ' * '
+╚[❖════════════❖]╝
+
+╔[❖════════════❖]╗
+             Admin Commands
+╚[❖════════════❖]╝
+
+ ❖ *kick <mention > ➾ kick member from server
+
+ ❖ *clear ➾ clear chat ( not ready yet fixing)
+ 
+ ❖ *mute < mention > ➾ mute member
+
+ ❖ *unmute <mention> ➾ unmute member
+
+ ❖ *ct <name> ➾ create channel
+
+ ❖ *cv <name> create voice channel
+  
+ ❖ *bc <message> ➾ message all members in server
+
+
+╔[❖════════════❖]╗
+            General  Commands
+╚[❖════════════❖]╝
+
+❖ *roll <number> ➾ role 
+
+❖ *date ➾ see date
+
+❖ *member ➾ members info
+
+❖ *ser-av ➾ server avatar
+
+❖ *uptime ➾ to see uptime
+
+❖ *invs ➾ server invite link
+
+❖ *own ➾ bot owner
+
+❖ *id ➾ your id
+
+❖ *avatar ➾ your avatar account
+
+❖ *help-en ➾ help by arabic
+
+❖ *help-en ➾ help by english
+
+❖ *ping ➾ to see ping
+
+❖ *bot ➾ bot informations 
+
+❖ *server ➾ server informations 
+
+❖ *invite ➾ bot invite link
+
+╔[❖════════════❖]╗
+                    Welcome
+╚[❖════════════❖]╝
+
+to enable welcome message do channel name "wlc"
+
+
+==================================================================
+
+Server support: رابط سيرفرك
+
+==================================================================
+
+bot invite link: رابط بوت مالتك
+
+==================================================================
+
+`);
+
+    }
+});
+
+client.on('message', message => {
     if (message.author.id === client.user.id) return;
     if (message.guild) {
    let embed = new Discord.RichEmbed()
